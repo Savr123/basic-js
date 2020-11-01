e77b271b6d311caed0a5aee1adf41e0069ea9844
Savr123 <Sangievv@mail.ru>
2020-11-01 20:47:02 +0300

    init: init solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -6,7 +6,7 @@ module.exports = class DepthCalculator {
       return 0;
     }
     if (!arr.length) {
-      arr.push(1);
+      return 1;
     }
     return 1 + Math.max(...arr.map((value) => this.calculateDepth(value)));
   }
